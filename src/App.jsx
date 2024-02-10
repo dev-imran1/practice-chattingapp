6d2cc22acc6fea76fe60966bcd609e69dd18a0da
dev-imran1 <freealltime247@gmail.com>
2024-02-11 00:01:22 +0600

    file setup and routing

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider
+} from "react-router-dom";
+import Registration from './pages/Registration';
+import Login from './pages/Login';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Registration />}/>
+      <Route path="/login" element={<Login />}/>
+    </Route>
+    ))
+
+function App() {
+
+  return (
+    <>
+        <RouterProvider router={router}
+  />
+    </>
+  )
+}
+
+export default App
