0e92c79ed27ad759d06c762f6c42e3272415eeb8
dev-imran1 <freealltime247@gmail.com>
2024-03-04 05:45:31 +0600

    friend frequest

--- src/component/frequest/FriendRequest.jsx
+++ src/component/frequest/FriendRequest.jsx
@@ -1,28 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../groupList/group.css'
 import profileimg from '../../assets/profile2.png'
 import { Button } from '@mui/material'
+import { getDatabase, onValue, ref} from "firebase/database";
+import { getAuth } from "firebase/auth";
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 
 const FriendRequest = () => {
+
+  let userData = useSelector((state) => state.logedUser.loginUser);
+  const auth = getAuth();
+  const db = getDatabase();
+  let [fRequestlist, setfRequestlist] = useState([]);
+
+  console.log(userData.uid )
+  useEffect(() => {
+    const userRef = ref(db, 'friendRequest/');
+    onValue(userRef, (snapshot) => {
+      const data = [];
+      snapshot.forEach((item) => {
+        if (userData.uid != item.key) {
+          data.push({ ...item.val(), id: item.key })
+        }
+      })
+      setfRequestlist(data);
+    });
+  }, []);
+
   return (
     <div className='manin__box'>
     <div className='title_friends'>
       <h3>Friend Request</h3>
     </div>
+  {fRequestlist.map((item)=>(
     <div className='item__wrapper'>
       <div className='item'>
         <div className="profile__picture">
-          <img src={profileimg} alt="" />
+          {/* <img src={console.log(item)} alt="" /> */}
+          <img src={item.whoreciveimg} alt="" />
         </div>
         <div className="profile__details">
-          <h3 className='profile__details-name'>Friends Reunion</h3>
-          <p className='profile__details-proffsion'>Web Devloper</p>
+          <h3 className='profile__details-name'>{item.whorecivename}</h3>
+          <p className='profile__details-proffsion'>{item.id}</p>
         </div>
         <div className="Request__btn">
         <Button variant="contained">add</Button>
         </div>
       </div>
       </div>
+  ))}
   </div>
   )
 }
--- src/component/userlist/UserList.jsx
+++ src/component/userlist/UserList.jsx
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react'
 import '../groupList/group.css';
 import profileimg from '../../assets/profile3.png'
 import { Button } from '@mui/material'
-import { getDatabase, ref, set, push, onValue } from "firebase/database";
+import { getDatabase, ref, set, push, onValue, remove } from "firebase/database";
 import { getAuth } from "firebase/auth";
 import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 
 const UserList = () => {
 
-  let userData = useSelector((state) => state.logedUser.loginUser)
-console.log(userData.uid)
+  let userData = useSelector((state) => state.logedUser.loginUser);
   const auth = getAuth();
   const db = getDatabase();
   let [userlist, setUserList] = useState([]);
@@ -22,7 +22,7 @@ console.log(userData.uid)
     onValue(userRef, (snapshot) => {
       const data = [];
       snapshot.forEach((item) => {
-        if(userData.uid != item.key){
+        if (userData.uid != item.key) {
           data.push({ ...item.val(), id: item.key })
         }
       })
@@ -41,14 +41,23 @@ console.log(userData.uid)
     });
   }, [])
 
-  // bujte parchi
+  // console.log(auth.currentUser.photoURL)
   let handelFrequest = (item) => {
-    // console.log(item)
-    set(ref(db, 'friendRequest/'+item.id), {
+    console.log(item);
+    set(ref(db, 'friendRequest/' + item.id), {
       whosendid: auth.currentUser.uid,
       whosendname: auth.currentUser.displayName,
+      whosendimg: auth.currentUser.photoURL,
       whoreciveid: item.id,
-      whorecivename: item.username
+      whorecivename: item.username,
+      whoreciveimg: item.profile_picture
+    })
+  }
+
+  let handelCancel = (item) => {
+    console.log("ami cancel", item)
+    remove(ref(db, 'friendRequest/'+item.id)).then(()=>{
+      toast("Delete Done");
     })
   }
 
@@ -62,15 +71,16 @@ console.log(userData.uid)
           <div className='item' >
             <div className="profile__picture">
               <img src={item.profile_picture} alt="" />
+              {/* <img src={console.log(item)} alt="" /> */}
             </div>
             <div className="profile__details">
               <h3 className='profile__details-name'>{item.username}</h3>
               <p className='profile__details-proffsion'>{item.email}</p>
             </div>
             <div className="Request__btn">
-              {friendrequest.includes(item.id+auth.currentUser.uid)
+              {friendrequest.includes(item.id + auth.currentUser.uid)
                 ?
-                <Button variant="contained">-</Button>
+                <Button onClick={() => handelCancel(item)} variant="contained">-</Button>
                 :
                 <Button onClick={() => handelFrequest(item)} variant="contained">send</Button>
               }
